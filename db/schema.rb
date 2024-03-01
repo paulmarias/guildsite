3d713bc6763242f614582a6e7db54306cacd87c5
paulmarias <paulmarias@hotmail.com>
2024-02-29 18:26:01 -0800

    catching up on migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_15_044333) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_01_022112) do
   create_table "bases", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -28,6 +28,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_15_044333) do
     t.string "title"
     t.text "body"
     t.string "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
